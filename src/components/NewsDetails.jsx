575ae3ccd0e7593af59fdf6ddd956abceea68f85
HamzaWebPro <hamza.mern2201@gmail.com>
2023-09-24 18:18:21 +0600

    post dynamic

--- src/components/NewsDetails.jsx
+++ src/components/NewsDetails.jsx
@@ -1,18 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+const formatDateTime = (createdAt) => {
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
+  return new Date(createdAt).toLocaleDateString(undefined, options);
+};
+
+const truncateText = (text, maxLength) => {
+  const words = text.split(" ");
+  if (words.length > maxLength) {
+    return words.slice(0, maxLength).join(" ") + "...";
+  } else {
+    return text;
+  }
+};
+
 const NewsDetails = ({ item }) => {
-    const {img, title, details, date, _id} = item;
+  console.log(item);
+
+  const truncatedContent = truncateText(item.content, 20);
+
   return (
-    <div className="space-y-5  shadow-md p-5" key={_id}>
+    <div className="space-y-5  shadow-md p-5" key={item._id}>
       <Link>
-        <img className="rounded" src={img} alt="" />
+        <img className="rounded w-full h-[250px]" src={item.image} alt="" />
       </Link>
-      <Link to={`/main_news/${_id}`} className="cursor-pointer">
-        <h2 className="font-bold text-[18px]">{title}</h2>
-      </Link>
-      <p className="text-slate-500">{details.slice(0, 100)}</p>
-      <p className="text-slate-500 text-[12px]">{date}</p>
+      <h2 className="font-bold text-[18px] ">
+        <Link to={"/"} className="cursor-pointer ">
+          {item.title}
+        </Link>
+      </h2>
+      <p className="text-slate-500">{truncatedContent}</p>
+      <small className="text-slate-500">{item.status === "Post by owner" ? "Post by owner" : item.status === "Post by superadmin" ? "Post by Superadmin" : "Post by Admin"}</small>
+      <p className="text-slate-500 text-[12px]">
+        {formatDateTime(item.createdAt)}
+      </p>
     </div>
   );
 };
--- src/pages/Home/News.jsx
+++ src/pages/Home/News.jsx
@@ -3,13 +3,21 @@ import "react-tabs/style/react-tabs.css"; // Import the default CSS styles
 import { Link } from "react-router-dom";
 import NewsDetails from "../../components/NewsDetails";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 const News = () => {
   const [news, setNews] = useState([]);
   useEffect(() => {
-    fetch("news.json")
-      .then((res) => res.json())
-      .then((data) => setNews(data));
+    async function getBlogs() {
+      await axios
+        .get("https://academy-backend-95ag.onrender.com/api/v1/blog/getBlog")
+        .then((res) => {
+          console.log("====================================");
+          setNews(res.data.blogs);
+          console.log("====================================");
+        });
+    }
+    getBlogs();
   }, []);
 
   const commiunity = news.filter((data) => data.category === "commiunity");
@@ -35,58 +43,79 @@ const News = () => {
 
         <TabPanel className="custom-tab-panel mt-10">
           <div className="lg:grid grid-cols-3 gap-5">
-            {news.map((item) => (
-              <NewsDetails item={item}/>
-            ))}
+            {!news
+              ? "Loading..."
+              : news.map((item) => <NewsDetails item={item} />)}
           </div>
         </TabPanel>
         <TabPanel className="custom-tab-panel mt-10">
           <div className="lg:grid grid-cols-3 gap-5">
-            {commiunity.map((item) => (
-              <NewsDetails item={item}/>
-            ))}
+            {!news
+              ? "Loading..."
+              : news.map(
+                  (item) =>
+                    item.category === "Community" && <NewsDetails item={item} />
+                )}
           </div>
         </TabPanel>
         <TabPanel className="custom-tab-panel mt-10">
           <div className="lg:grid grid-cols-3 gap-5">
-            {featured.map((item) => (
-              <NewsDetails item={item}/>
-            ))}
+            {!news
+              ? "Loading..."
+              : news.map(
+                  (item) =>
+                    item.category === "Featured" && <NewsDetails item={item} />
+                )}
           </div>
         </TabPanel>
         <TabPanel className="custom-tab-panel mt-10">
           <div className="lg:grid grid-cols-3 gap-5">
-            {newses.map((item) => (
-              <NewsDetails item={item}/>
-            ))}
+            {!news
+              ? "Loading..."
+              : news.map(
+                  (item) =>
+                    item.category === "News" && <NewsDetails item={item} />
+                )}
           </div>
         </TabPanel>
         <TabPanel className="custom-tab-panel mt-10">
           <div className="lg:grid grid-cols-3 gap-5">
-            {academy.map((item) => (
-              <NewsDetails item={item}/>
-            ))}
+          {!news
+              ? "Loading..."
+              : news.map(
+                  (item) =>
+                    item.category === "Academy" && <NewsDetails item={item} />
+                )}
           </div>
         </TabPanel>
         <TabPanel className="custom-tab-panel mt-10">
           <div className="lg:grid grid-cols-3 gap-5">
-            {blockchain.map((item) => (
-              <NewsDetails item={item}/>
-            ))}
+          {!news
+              ? "Loading..."
+              : news.map(
+                  (item) =>
+                    item.category === "Blockchain" && <NewsDetails item={item} />
+                )}
           </div>
         </TabPanel>
         <TabPanel className="custom-tab-panel mt-10">
           <div className="lg:grid grid-cols-3 gap-5">
-            {payments.map((item) => (
-              <NewsDetails item={item}/>
-            ))}
+          {!news
+              ? "Loading..."
+              : news.map(
+                  (item) =>
+                    item.category === "Web3 Payments" && <NewsDetails item={item} />
+                )}
           </div>
         </TabPanel>
         <TabPanel className="custom-tab-panel mt-10">
           <div className="lg:grid grid-cols-3 gap-5">
-            {developers.map((item, index) => (
-              <NewsDetails item={item}/>
-            ))}
+          {!news
+              ? "Loading..."
+              : news.map(
+                  (item) =>
+                    item.category === "Developers" && <NewsDetails item={item} />
+                )}
           </div>
         </TabPanel>
       </Tabs>
