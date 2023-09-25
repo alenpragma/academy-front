c989c834c946a9719f5fd09b3c47975e51e03d2c
Remon Tripura <remontripura045@gmail.com>
2023-09-25 15:46:51 +0600

    hover section update

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,9 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routers/router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-<<<<<<< HEAD
   <div className="max-w-6xl lg:mx-auto md:mx-5">
-=======
-
->>>>>>> 9fc6660e31c73d5328e264133742e9a461139dcf
     <React.StrictMode>
       <RouterProvider router={router} />
     </React.StrictMode>
-
+  </div>
 );
